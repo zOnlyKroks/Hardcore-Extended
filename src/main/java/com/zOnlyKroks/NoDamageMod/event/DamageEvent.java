87c2574f519d544f3081c26f29e637f4badf5a33
zOnlyKroks <64548817+zOnlyKroks@users.noreply.github.com>
2021-01-02 17:24:45 +0100

    Update DamageEvent.java

--- src/main/java/com/zOnlyKroks/NoDamageMod/event/DamageEvent.java
+++ src/main/java/com/zOnlyKroks/NoDamageMod/event/DamageEvent.java
@@ -1,7 +1,9 @@
 package com.zOnlyKroks.NoDamageMod.event;
 
 import com.zOnlyKroks.NoDamageMod.NoDamageMod;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.storage.SaveFormat;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
