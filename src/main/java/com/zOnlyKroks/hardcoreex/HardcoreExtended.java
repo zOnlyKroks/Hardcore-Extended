195ede6a98aecf0d0496c4f722c02c0b1863aa6f
zOnlyKroks <64548817+zOnlyKroks@users.noreply.github.com>
2021-01-13 16:12:11 +0100

    Only Fish Challange

--- src/main/java/com/zOnlyKroks/hardcoreex/HardcoreExtended.java
+++ src/main/java/com/zOnlyKroks/hardcoreex/HardcoreExtended.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
